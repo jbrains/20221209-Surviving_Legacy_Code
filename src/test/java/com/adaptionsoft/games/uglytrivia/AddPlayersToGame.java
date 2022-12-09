853c873aa89080ccfcf2bab063b1aab2b0d538dd
J. B. Rainsberger <me@jbrains.ca>
2022-12-09 13:37:09 -0400

    Documented defect JBRAINS-720: we fail when we try to add the 6th player to the Game.

--- src/test/java/com/adaptionsoft/games/uglytrivia/AddPlayersToGame.java
+++ src/test/java/com/adaptionsoft/games/uglytrivia/AddPlayersToGame.java
@@ -14,4 +14,18 @@ public class AddPlayersToGame {
 
         Assert.assertEquals(Arrays.asList("player 1"), game.players);
     }
+
+    @Test
+    public void six() {
+        final Game game = new Game();
+        for (int i = 1; i <= 5; i++) {
+            game.add(String.format("player %d", i));
+        }
+
+        try {
+            game.add("player 6");
+            Assert.fail("Defect JBRAINS-720 appears to have been fixed.");
+        } catch (ArrayIndexOutOfBoundsException expected) {
+        }
+    }
 }
